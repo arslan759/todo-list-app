9f9a6d0a5d7eb117b0e16c0a8d8c899de67c2db7
arslan759 <arslan.obaid759@gmail.com>
2022-11-16 20:59:54 +0500

    project added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
 
 function App() {
+  const [add, setAdd] = useState([]);
+  const [task, setTask] = useState();
+
+  function handleChange(event) {
+    setTask(event.target.value);
+  }
+  function addTask() {
+    task ? setAdd((e) => [task, ...e]) : alert("Please add a Task");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container fluid">
+      <div className="main-app">
+        <div className="container d-flex justify-content-center my-2">
+          <h2>To-Do List</h2>
+        </div>
+        <form className="" onSubmit={(e) => e.preventDefault()}>
+          <div className="row d-flex justify-content-between">
+            <div className="col-md-10">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter a task..."
+                onChange={handleChange}
+              ></input>
+            </div>
+            <div className="col-md-2 mx-0">
+              <Button variant="primary" onClick={addTask}>
+                Add
+              </Button>
+            </div>
+          </div>
+        </form>
+        <div className="container mt-3">
+          {add.map((item) => (
+            <div className="row task">
+              <div className="col">{item}</div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
